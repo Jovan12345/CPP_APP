8fa04f9239e70344453195dc5c3a609ddbbe498e
Jovan12345 <jovanj@gecko.mk>
2020-11-02 17:45:28 +0100

    added geo location

--- src/sagas/saga.js
+++ src/sagas/saga.js
@@ -1,14 +1,27 @@
-import { put, takeEvery } from 'redux-saga/effects';
+import { call, put, takeEvery } from 'redux-saga/effects';
+import geoLocation from '../apis/geoLocationAddress';
 
 function* addPhotoAsync(action) {
-    try {
-      yield put({type: 'ADD_PHOTO_ASYNC', payload: action.payload});
-    } catch (e) {
-      console.log('Error', e);
-    }
+  try {
+    yield put({ type: 'ADD_PHOTO_ASYNC', payload: action.payload });
+  } catch (e) {
+    console.log('Error', e);
   }
+}
+
+function* takeAddressAPI(action) {
+  try {
+    yield call(geoLocation)
+  } catch (e) {
+    console.log('Error', e);
+  }
+}
 
 export function* watchAddPhoto() {
-    yield takeEvery('ADD_PHOTO', addPhotoAsync);
+  yield takeEvery('ADD_PHOTO', addPhotoAsync);
+}
+
+export function* watchPhotoLocation() {
+  yield takeEvery('ADD_ADDRESS', takeAddressAPI)
 }
 
